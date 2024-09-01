72668642d5c785e6292ba9e13d23233ffffc41b5
fanwwi <farangiz.anvarjanova553@gmail.com>
2024-09-01 16:36:47 +0600

    .

---
+++ accounts/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "password", "confirm_password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["email"],
+            password=validated_data["password"],
+            email=validated_data["confirm_password"],
+        )
+        return user
---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from rest_framework import generics
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key})
+        return Response({"error": "Invalid Credentials"}, status=400)
